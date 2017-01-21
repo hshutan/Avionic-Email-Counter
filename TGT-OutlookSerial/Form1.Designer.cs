335eb2dffe2c8bbabccde432c4cd327622ee76bc
hshutan <hshutan@gmail.com>
2017-01-21 12:54:48 -0600

    Init commit of Windows C# software

---
+++ TGT-OutlookSerial/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace TGT_OutlookSerial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.cboComPort = new System.Windows.Forms.ComboBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // cboComPort
+            // 
+            this.cboComPort.FormattingEnabled = true;
+            this.cboComPort.Location = new System.Drawing.Point(12, 12);
+            this.cboComPort.Name = "cboComPort";
+            this.cboComPort.Size = new System.Drawing.Size(80, 21);
+            this.cboComPort.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(99, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Start";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Enabled = false;
+            this.btnClose.Location = new System.Drawing.Point(177, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Stop";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Enabled = false;
+            this.btnReset.Location = new System.Drawing.Point(256, 12);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker2_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 48);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.cboComPort);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TGT Outlook Integration";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ComboBox cboComPort;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnReset;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+    }
+}
+
---
+++ TGT-OutlookSerial/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OutLook = Microsoft.Office.Interop.Outlook;
+using System.IO.Ports;
+
+namespace TGT_OutlookSerial
+{
+    public partial class Form1 : Form
+    {
+
+        int lastNumUnread = 0;
+        int lastNumTotal = 0;
+        bool shouldBeRunning = false;
+        bool resetCalledFor = false;
+        bool portFailed = false;
+        string myPort = "";
+        int failSafeCounter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                cboComPort.Items.Clear();
+                string[] ports = SerialPort.GetPortNames();
+                foreach (string port in ports)
+                {
+                    cboComPort.Items.Add(port);
+                    cboComPort.Text = port;
+                }
+
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                btnReset.Enabled = true;
+                btnOpen.Enabled = false;
+                btnClose.Enabled = true;
+                shouldBeRunning = true;
+                serialPort1.PortName = cboComPort.Text;
+                myPort = cboComPort.Text;
+                serialPort1.BaudRate = 9600;
+                serialPort1.Open();
+                backgroundWorker1.RunWorkerAsync();
+                backgroundWorker2.RunWorkerAsync();
+            }
+            catch (Exception)
+            {
+                lastNumUnread = -1;
+                lastNumTotal = -1;
+                // Error starting.
+            }
+
+
+        }
+
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    if (resetCalledFor)
+                    {
+                        serialPort1.WriteLine("r");
+                        resetCalledFor = false;
+                    }
+                    else
+                    {
+
+                        // Connect to Microsoft Outlook
+
+                        OutLook.Application oApp;
+                        OutLook._NameSpace oNS;
+
+                        oApp = new OutLook.Application();
+                        oNS = (OutLook._NameSpace)oApp.GetNamespace("MAPI");
+
+                        // Get the number of unread Inbox items
+                        int numUnread = oNS.GetDefaultFolder(Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderInbox).UnReadItemCount;
+                        // Get the total number of Inbox items
+                        int numTotal = oNS.GetDefaultFolder(Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderInbox).Items.Count;
+
+                        if (numUnread > 10)
+                        {
+                            numUnread = 10;
+                        }
+
+                        if (numTotal > 999)
+                        {
+                            numTotal = 999;
+                        }
+
+                        string myAPI = numUnread.ToString() + "," + numTotal.ToString() + ".";
+
+
+
+                        if ((lastNumUnread != numUnread) || (lastNumTotal != numTotal))
+                        {
+                            // We need to update the display.
+                            // Tell the device what to display
+                            serialPort1.WriteLine(myAPI);
+                            // Remember what we just sent, as to not re-send when numbers have not changed
+                            lastNumUnread = numUnread;
+                            lastNumTotal = numTotal;
+                            // Prevent high CPU usage with delay
+                            System.Threading.Thread.Sleep(1000);
+                        }
+                        else
+                        {
+                            // We don't need to update the display.
+                            failSafeCounter++;
+                            // Prevent high CPU usage with delay
+                            System.Threading.Thread.Sleep(500);
+                        }
+
+                        if (failSafeCounter > 5)
+                        {
+                            // Cause program to re-send unread and total items every so often even when data has not changed
+                            lastNumUnread = -1;
+                            lastNumTotal = -1;
+                            failSafeCounter = 0;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    System.Threading.Thread.Sleep(5000);
+                    lastNumUnread = -1;
+                    lastNumTotal = -1;
+                }
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (shouldBeRunning)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            lastNumUnread = -1;
+            lastNumTotal = -1;
+            try
+            {
+                serialPort1.WriteLine("0,0.");
+                serialPort1.WriteLine("b");
+            }
+            catch (Exception)
+            {
+            }
+            try
+            {
+                btnReset.Enabled = false;
+                btnOpen.Enabled = true;
+                btnClose.Enabled = false;
+                shouldBeRunning = false;
+                serialPort1.Close();
+            }
+            catch (Exception)
+            {
+                
+            }
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            resetCalledFor = true;
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                string[] ports = SerialPort.GetPortNames();
+                if (!ports.Contains(myPort))
+                {
+                    portFailed = true;
+                    shouldBeRunning = false;
+                    lastNumUnread = -1;
+                    lastNumTotal = -1;
+                }
+                if (ports.Contains(myPort))
+                {
+                    portFailed = false;
+                    shouldBeRunning = true;
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                if (portFailed)
+                {
+                    lastNumUnread = -1;
+                    lastNumTotal = -1;
+                    System.Threading.Thread.Sleep(1000);
+                    serialPort1.Open();
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    portFailed = false;
+                    shouldBeRunning = true;
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        private void backgroundWorker2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            backgroundWorker2.RunWorkerAsync();
+        }
+    }
+}
